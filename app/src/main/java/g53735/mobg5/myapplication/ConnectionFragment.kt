92d832c0fb0a78c8d063f7684915d2db70802e5b
Bryan Grégoire <53735@etu.he2b.be>
2021-10-24 20:12:46 +0200

    button check email is valid

--- app/src/main/java/g53735/mobg5/myapplication/ConnectionFragment.kt
+++ app/src/main/java/g53735/mobg5/myapplication/ConnectionFragment.kt
@@ -1,11 +1,13 @@
 package g53735.mobg5.myapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Patterns.EMAIL_ADDRESS
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import g53735.mobg5.myapplication.databinding.FragmentConnectionBinding
 
 /**
@@ -19,8 +21,22 @@ class ConnectionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentConnectionBinding>(inflater, R.layout.fragment_connection, container, false)
+        val binding = DataBindingUtil.inflate<FragmentConnectionBinding>(
+            inflater,
+            R.layout.fragment_connection,
+            container,
+            false
+        )
+
+        binding.connectionButton.setOnClickListener { isValidEmail(binding.emailEditText.text.toString())}
+
         return binding.root
+    }
+
+    private fun isValidEmail(email : String) {
+        if(!EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(context, "Email invalide", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
