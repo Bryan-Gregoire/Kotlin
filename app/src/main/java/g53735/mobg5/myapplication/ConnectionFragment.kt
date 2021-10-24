3ac7b3dea783158bc9bc89b45599cdf1e33d9b0d
Bryan Grégoire <53735@etu.he2b.be>
2021-10-24 22:27:22 +0200

    remove focus from email edit and display state email valide

--- app/src/main/java/g53735/mobg5/myapplication/ConnectionFragment.kt
+++ app/src/main/java/g53735/mobg5/myapplication/ConnectionFragment.kt
@@ -1,10 +1,12 @@
 package g53735.mobg5.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Patterns.EMAIL_ADDRESS
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -12,11 +14,13 @@ import g53735.mobg5.myapplication.databinding.FragmentConnectionBinding
 
 class ConnectionFragment : Fragment() {
 
+    private lateinit var binding: FragmentConnectionBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentConnectionBinding>(
+        binding = DataBindingUtil.inflate<FragmentConnectionBinding>(
             inflater,
             R.layout.fragment_connection,
             container,
@@ -33,7 +37,13 @@ class ConnectionFragment : Fragment() {
     private fun isValidEmail(email: String) {
         if (!EMAIL_ADDRESS.matcher(email).matches()) {
             Toast.makeText(context, "Email invalide", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Email valide", Toast.LENGTH_SHORT).show()
         }
+        val inputMethodManager =
+            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE)
+                    as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.connectionButton.windowToken, 0)
+        binding.emailEditText.clearFocus()
     }
-
 }
