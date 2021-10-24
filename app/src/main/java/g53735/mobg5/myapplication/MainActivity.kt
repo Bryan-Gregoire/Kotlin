56da366b6ba14333db580af0a5881dd442a2fd82
Bryan Grégoire <53735@etu.he2b.be>
2021-10-24 18:53:59 +0200

    filling fragments and add navigation drawer

--- app/src/main/java/g53735/mobg5/myapplication/MainActivity.kt
+++ app/src/main/java/g53735/mobg5/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package g53735.mobg5.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import g53735.mobg5.myapplication.databinding.ActivityMainBinding
@@ -10,10 +11,21 @@ import g53735.mobg5.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
 
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
 }
